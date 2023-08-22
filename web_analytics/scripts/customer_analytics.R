4413a3469c9e5cf6fc7e5478bd88949001600291
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-08-22 19:00:33 -0400

    within campaign percent checkpoint

--- web_analytics/scripts/customer_analytics.R
+++ web_analytics/scripts/customer_analytics.R
@@ -33,7 +33,7 @@ leads_tbl <- readxl::read_excel(
     mutate(sign_up_date = ymd(sign_up_date)) %>% 
     mutate(date_of_1st_purchase = ymd(date_of_1st_purchase)) %>% 
     
-    # days to purchase
+    # add days to purchase column
     mutate(
         days_to_purchase = difftime(
             time1 = sign_up_date, 
@@ -42,7 +42,7 @@ leads_tbl <- readxl::read_excel(
         ) %>% as.numeric %>% abs()
     ) %>% 
     
-    # number of touch points
+    # add number of touch points column
     mutate(touch_points = case_when(
         purch_30d_post_web == 1   ~ 1,
         purch_30d_post_email == 1 ~ 2,
@@ -61,8 +61,28 @@ survey_tbl <- readxl::read_excel(
 ) %>% 
     clean_names() %>% 
     select(-c(x8:x11)) %>% 
-    `colnames<-`(c("lead_source", "age", "age_group", "recommend", "hear_about",
-                   "re_purchase", "re_purchase_reason"))
+    
+    # shorten names of survey questions
+    `colnames<-`(
+        c(
+            "lead_source", 
+            "age", 
+            "age_group", 
+            "recommend", 
+            "hear_about",
+            "re_purchase", 
+            "re_purchase_reason"
+        )
+    ) %>% 
+    
+    # add age groups column
+    mutate(age_group = case_when(
+        age %>% between(18, 25) ~ "18 - 25",
+        age %>% between(26, 35) ~ "26 - 35",
+        age %>% between(36, 45) ~ "36 - 45",
+        age %>% between(46, 55) ~ "46 - 55",
+        TRUE                    ~ "55+"
+    ))
 
 survey_tbl %>% glimpse()
 
@@ -75,10 +95,7 @@ campaign_tbl <- get_campaign_data()
 # CUSTOMER STATS ----
 # *****************************************************************************
 
-# * Functions ----
-
-# - Customer Metrics
-
+# * Customer Metrics Table ----
 customer_metrics_tbl <- leads_tbl %>% 
     filter(total_1yr_purch_net > 0) %>% 
     select(lead_source, days_to_purchase, total_1yr_purch_net, touch_points) %>% 
@@ -96,6 +113,14 @@ customer_metrics_tbl <- leads_tbl %>%
         .by = campaign
     ) %>% 
     arrange(campaign) %>% 
+    
+    # average customer age
+    left_join(
+        survey_tbl %>% 
+            summarise(avg_age = mean(age, na.rm = TRUE), .by = lead_source) %>% 
+            add_row(lead_source = "Total", avg_age = mean(survey_tbl$age)),
+        by = c("campaign" = "lead_source")
+    ) %>% 
     
     # customer acquisition cost
     left_join(
@@ -129,8 +154,59 @@ customer_metrics_tbl
 
 # *****************************************************************************
 # **** ----
-# SECTION NAME ----
+# SURVEY DATA DEEP DIVE ----
 # *****************************************************************************
+
+# * Customer Demographics (Total) ----
+survey_tbl %>% 
+    count(age_group) %>% 
+    mutate(pct = n / sum(n))
+
+# * Customer Demographics ----
+age_demographics_tbl <- survey_tbl %>% 
+    select(lead_source, age_group) %>% 
+    mutate(total = "Total") %>% 
+    pivot_longer(
+        cols      = -(age_group),
+        names_to  = "name",
+        values_to = "campaign"
+    ) %>% 
+    summarise(count = n(), .by = c(campaign, age_group))
+
+# * Raw Volume ----
+age_demographics_tbl %>% 
+    pivot_wider(
+        names_from  = age_group,
+        values_from = count
+    ) %>% 
+    arrange(campaign)
+
+# * Within Age Group PCT ----
+age_demographics_tbl %>% 
+    filter(!campaign == "Total") %>% 
+    group_by(age_group) %>% 
+    mutate(total = count/sum(count)) %>% 
+    ungroup() %>% 
+    select(-count) %>% 
+    pivot_wider(
+        names_from = age_group,
+        values_from = total
+    )
+    
+
+# * Within Campaign Percent ----
+age_demographics_tbl %>% 
+    group_by(campaign) %>% 
+    mutate(pct = count/sum(count)) %>% 
+    ungroup() %>% 
+    select(-count) %>% 
+    pivot_wider(
+        names_from  = age_group,
+        values_from = pct
+    ) %>% 
+    arrange(campaign)
+
+
 # *****************************************************************************
 # **** ----
 # SECTION NAME ----
