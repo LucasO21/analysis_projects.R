6a618ac02dd20baf124a0f11f811f3fcd3dc712b
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-08-25 06:48:20 -0400

    checkpoint; paid assumptions data #02

--- web_analytics/scripts/03_modeling_analysis.R
+++ web_analytics/scripts/03_modeling_analysis.R
@@ -51,6 +51,27 @@ campaign_tbl <- read_rds("../data/data_clean/campaign_data.rds")
 # * Repeat Customers Data ----
 customer_retention_tbl <- read_rds("../data/data_clean/customer_retention_data.rds")
 
+# * Customer Metrics ----
+customer_metrics_tbl <- get_customer_analysis_final_output(
+    data_leads  = leads_tbl,
+    data_survey = survey_tbl,
+    output      = "customer_metrics"
+)
+
+# * PPC Metrics ----
+ppc_metrics_tbl <- get_ppc_final_output(
+    data   = campaign_tbl,
+    level  = "campaign",
+    output = "metrics"
+)
+
+# * Customer Retention ----
+customer_retention_tbl <- get_customer_analysis_final_output(
+    data_leads  = leads_tbl,
+    data_survey = survey_tbl,
+    output      = "customer_retention"
+)
+
 
 # *****************************************************************************
 # **** ----
@@ -59,14 +80,14 @@ customer_retention_tbl <- read_rds("../data/data_clean/customer_retention_data.r
 
 # - Create a table with estimates (assumptions) for clicks, leads, cost, etc
 #   from other channels like linkedin, spotify and tiktok, assuming we decide to
-#   create campaigns for those platforms.
+#   create ad campaigns for those platforms.
 
 # * New Ad Channels Data ----
 new_ad_channels_tbl <- tibble(
     campaign        = c("Linkedin", "Spotify", "TikTok"),
-    total_clicks    = c(800, 100000, 1000),
+    total_clicks    = c(800, 100000, 4000),
     total_leads     = c(75, 1500, 150),
-    total_cost      = c(6000, 17000, 2000),
+    total_cost      = c(6000, 17000, 6000),
     total_customers = c(0.40 * 75, 0.28 * 1500, 0.28 * 500),
     avg_1yr_revenue = c(335, 330, 335),
     retention_rate  = c(0.70, 0.62, 0.68)
@@ -90,12 +111,7 @@ new_channels_clv_estimate_tbl <- ppc_metrics_tbl %>%
         customer_metrics_tbl %>% 
             select(campaign, total_customers, avg_1yr_revenue)
     ) %>% 
-    left_join(
-        customer_retention_tbl %>% 
-            filter(repeat_purchase == "Repeat") %>% 
-            select(-repeat_purchase) %>% 
-            gather(key = "campaign", value = "retention_rate")
-    ) %>% 
+    left_join(customer_retention_tbl) %>% 
     
     # remove total row
     filter(campaign != "Total") %>% 
@@ -104,7 +120,7 @@ new_channels_clv_estimate_tbl <- ppc_metrics_tbl %>%
     bind_rows(new_ad_channels_tbl) %>% 
     
     # lead conversion rate
-    mutate(click_cvr = total_customers / total_clicks) %>% 
+    mutate(ctc_cvr = total_customers / total_clicks) %>% 
     
     # cac
     mutate(cac = total_cost / total_customers) %>% 
@@ -135,54 +151,60 @@ budget <- 1000
 new_customer_per_1000_tbl <- new_channels_clv_estimate_tbl %>% 
     mutate(cpc = total_cost / total_clicks) %>% 
     mutate(est_clicks = budget / cpc) %>% 
-    mutate(est_customers = click_cvr * est_clicks)
+    mutate(est_customers = ctc_cvr * est_clicks)
 
 new_customer_per_1000_tbl %>% glimpse()
-
-
 
 
 # *****************************************************************************
 # **** ----
 # CASH FLOW ASSUMPTIONS ----
 # *****************************************************************************
-
-# * Scenario 1 ----
 new_customer_per_1000_tbl %>% glimpse()
 
+#' - Here we'll be modeling with 2 scenarios for new ad campaigns;
+#' - In scenario 0, we will not take on any additional marketing cost.
+#' - In scenario 1, we'll hire a new marketing manager to manage campaign ads.
+
+
+# * Scenario Inputs ----
+
 # ** Marketing Costs ----
-scenario                  <- 1
+scenario                  <- 1 # hire new marketing manager
 marketing_manager_salary  <- 80000
 current_salary_allocation <- 0.20
-social_media_tools_cost   <- 250 # per month
+social_media_tools_cost   <- 250 # $250 per month
 channel_list <- c("AdWords", "Facebook", "Linkedin", "Spotify", "TikTok")
 
 
 # ** Paid Customer Acquisition ----
 paid_assumptions_inputs_tbl <- tibble(
-    campaign                  = channel_list,
-    monthly_ad_spend          = c(2000, 4000, 2000, 5000, 4000),
-    est_click_cvr_uplift      = c(0.02, 0.03, 0, 0, 0),
+    campaign            = channel_list,
+    monthly_ad_spend    = c(2000, 4000, 2000, 5000, 4000),
+    est_ctc_cvr_uplift  = c(0.02, 0.03, 0, 0, 0),
     est_retention_rate_uplift = c(0.02, 0.03, 0.02, 0.02, 0.03)
 )
 
 
 paid_assumptions_tbl <- new_customer_per_1000_tbl %>% 
-    select(campaign, cpc, retention_rate, click_cvr) %>% 
-    left_join(
-        paid_assumptions_tbl
-    ) %>% 
+    select(campaign, cpc, retention_rate, ctc_cvr) %>% 
+    left_join(paid_assumptions_inputs_tbl) %>% 
     
+    # estimated clicks
     mutate(est_clicks = monthly_ad_spend / cpc) %>% 
-    mutate(est_new_click_cvr = case_when(
-        scenario == 0 ~ click_cvr,
-        TRUE          ~ click_cvr + est_click_cvr_uplift
+    
+    # estimated uplift in click to customer cvr based on scenario
+    mutate(est_new_ctc_cvr = case_when(
+        scenario == 0 ~ ctc_cvr,
+        TRUE          ~ ctc_cvr + est_ctc_cvr_uplift
     )) %>% 
-    mutate(est_new_customers = est_new_click_cvr * est_clicks) %>% 
+    
+    # estimated uplift in customer retention rate
+    mutate(est_new_customers = est_new_ctc_cvr * est_clicks) %>% 
     mutate(est_new_retention_rate = case_when(
         scenario == 0 ~ retention_rate,
         TRUE          ~ retention_rate + est_retention_rate_uplift
-    ))
+    )) 
     
     
 
